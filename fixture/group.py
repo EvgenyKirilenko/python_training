1f81b9333785810cd23f3148c2c15ca81e4941ac
Eugene <260801@gmail.com>
2019-11-15 23:52:36 +0300

    finally something works needs cleaning

--- fixture/group.py
+++ fixture/group.py
@@ -7,7 +7,6 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("submit").click()
 
-
     def return_to_group_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
@@ -21,8 +20,7 @@ class GroupHelper:
         self.open_groups_page()
         self.open_new_group_page()
         self.fill_form(group)
-        #self.click_submit()
-        #wd.find_element_by_name("submit").click()
+        self.click_submit()
 
     def edit_first_group(self, group):
         wd = self.app.wd
@@ -30,6 +28,14 @@ class GroupHelper:
         self.select_first_group()
         self.click_edit()
         self.fill_form(group)
+        wd.find_element_by_name("update").click()
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        self.click_edit()
+        self.fill_group_form(new_group_data)
         wd.find_element_by_name("update").click()
 
     def click_edit(self):
@@ -40,15 +46,23 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
     def fill_form(self, group):
         wd = self.app.wd
-        self.change_filed_value("group name", group.name)
-        self.change_filed_value("group header", group.header)
-        self.change_filed_value("group footer", group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
 
-    def change_filed_value(self, field_name, text):
+    def change_field_value(self, field_name, text):
         wd = self.app.wd
         if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
     def open_groups_page(self):
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,16 +1,19 @@
 from model.group import Group
 
+
 def test_edit_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="new group"))
+    app.group.fill_group_form(Group(name="new group"))
     app.group.return_to_group_page()
     app.session.logout()
+
 
 def test_edit_group_header(app):
     app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(header="new header"))
     app.group.return_to_group_page()
     app.session.logout()
+
 
 def test_edit_group_footer(app):
     app.session.login(username="admin", password="secret")
